4e5654e4aa9811ae68d54e5992175cf17d5352e5
ellisgl <xelliss@gmail.com>
2021-11-10 10:47:04 -0600

    Adding Whoops and simple test.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,4 +2,29 @@
 
 declare(strict_types=1);
 
-echo 'Hello World!';
+namespace App;
+
+use \Exception;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new Exception;
