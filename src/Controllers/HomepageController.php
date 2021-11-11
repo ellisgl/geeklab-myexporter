7ce968716d65da2536c3d51201cb7718cf8b39da
ellisgl <xelliss@gmail.com>
2021-11-11 12:31:39 -0600

    Template rendering.

--- src/Controllers/HomepageController.php
+++ src/Controllers/HomepageController.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Core\BaseController;
+
 class HomepageController extends BaseController
 {
     public function index(): void
     {
-        $this->response->setContent('Controller Injected "Hello, World!"');
+        $data = [
+            // 'name' => $this->request->getParameter('name', 'stranger'),
+            'name' => $this->request->query->get('name', 'strange'),
+        ];
+        $html = $this->renderer->render('Homepage', $data);
+        $this->response->setContent($html);
     }
 
     public function test(): void
